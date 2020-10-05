38906af05e73030706b7ff8ed48c01776cec4454
smbdawsm <nikolay.nikishev@icloud.com>
2020-10-06 01:23:38 +0700

    ex1-3

---
+++ 1st_lesson.py
@@ -0,0 +1,8 @@
+a = 10
+b = 17
+print(a, ' this is A')
+print('this is B', b)
+a = input(" input A pls: ")
+string = input('input a string pls: ')
+print ('new A', a)
+print ('a string: ', string)
---
+++ ex3.py
@@ -0,0 +1,7 @@
+n = input('please input n: ')
+nn = int(n+n)
+nnn = int (n+n+n)
+int_n = int(n)
+sum = int_n + nn + nnn
+print ('Answer = ', sum)
+
