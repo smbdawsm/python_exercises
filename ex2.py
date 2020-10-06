f16f89c4bc6cebd29a3d53b980b6a364966bda05
smbdawsm <nikolay.nikishev@icloud.com>
2020-10-06 20:41:04 +0700

    formast time

--- ex2.py
+++ ex2.py
@@ -3,5 +3,5 @@ minuts = time_in_second // 60
 seconds = time_in_second % 60
 hours = minuts // 60
 minuts_for_answer = minuts % 60
-print(f"{hours}:{minuts_for_answer}:{seconds}")
+print(f"{hours:02.0f}:{minuts_for_answer:02.0f}:{seconds:02.0f}")
 
