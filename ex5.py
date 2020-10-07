b98bb420132f310e091322890686c0d3c2b09fd8
smbdawsm <nikolay.nikishev@icloud.com>
2020-10-07 12:03:28 +0700

    округлил

--- ex5.py
+++ ex5.py
@@ -3,8 +3,8 @@ rashod = int(input ('Izderjka = '))
 if dohod == rashod:
     print('Firma nichego ne zarabotala')
 elif dohod >= rashod:
-    print(f'Firma zarabotala {dohod-rashod}, rentabelnost {(dohod-rashod)/dohod}')
+    print(f'Firma zarabotala {dohod-rashod:.02f}, rentabelnost {(dohod-rashod)/dohod:.02f}')
     num = int(input('A skolko sotrudnikov v firme? '))
-    print(f'Kajdiy sotrudnik zarabotal {(dohod-rashod)/num}')
+    print(f'Kajdiy sotrudnik zarabotal {(dohod-rashod)/num:.02f}')
 else:
     print(f'Firma poteryala {rashod-dohod}')
