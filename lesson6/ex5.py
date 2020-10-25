ed8bc4ec54fb6eea7bfdf5b4a16e2a511b941460
Nikolay Nikishev <nikolay.nikishev@icloud.com>
2020-10-25 16:58:36 +0700

    initial commit

---
+++ lesson6/ex5.py
@@ -0,0 +1,45 @@
+'''
+Реализовать класс Stationery (канцелярская принадлежность). Определить в нем атрибут title (название) и метод draw (отрисовка).
+Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер). 
+В каждом из классов реализовать переопределение метода draw. Для каждого из классов методы должен выводить уникальное сообщение.
+Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+'''
+
+class Stationery():
+
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return ('Запуск отрисовки')
+
+class Pen(Stationery):
+
+    def __init__(self,title):
+        super().__init__(title)
+
+    def draw(self):
+        return ('this is pen')
+
+class Pencil(Stationery):
+
+    def __init__(self,title):
+        super().__init__(title)
+
+    def draw(self):
+        return ('this is pencil')
+
+class Handle(Stationery):
+
+    def __init__(self,title):
+        super().__init__(title)
+
+    def draw(self):
+        return ('this is Handle')
+
+a = Pen('2H')
+b = Pencil('2B')
+c = Handle('3M')
+print(a.draw())
+print(b.draw())
+print(c.draw())
